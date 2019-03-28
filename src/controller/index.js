21612f919472c1490bfddefdc7510c4c7af633cd
higor.galdino <hbgaldino@gmail.com>
2019-03-28 13:48:52 -0300

    remaining routes

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,39 @@
 const getSchedules = (req, res, next) => {
   res.sendStatus(200);
   next();
-}
+};
+
+const getSchedule = (req, res, next) => {
+  res.sendStatus(200);
+  next();
+};
+
+const deleteSchedule = (req, res, next) => {
+  res.sendStatus(200);
+  next();
+};
+
+const postSchedule = (req, res, next) => {
+  res.sendStatus(200);
+  next();
+};
+
+const putSchedule = (req, res, next) => {
+  res.sendStatus(200);
+  next();
+};
+
+const patchSchedule = (req, res, next) => {
+  res.sendStatus(200);
+  next();
+};
+
 
 module.exports = { 
-  getSchedules
+  getSchedules,
+  getSchedule,
+  deleteSchedule,
+  postSchedule,
+  putSchedule,
+  patchSchedule
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const busController = require("../controller");
 
-
 router.get("/schedules", busController.getSchedules);
+router.get("/schedules/:id", busController.getSchedule);
+router.delete('/schedules/:id', busController.deleteSchedule);
+router.post("/schedules", busController.postSchedule);
+router.put("/schedules/:id", busController.putSchedule);
+router.patch("/schedules/:id", busController.patchSchedule);
 
 module.exports = router;
