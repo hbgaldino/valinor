5e08c7c790c6753332d5e3923311f806f8179682
higor.galdino <hbgaldino@gmail.com>
2019-04-05 15:17:11 -0300

    delete operation

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,26 +1,56 @@
 import ScheduleModel from '../model/schedule';
 
-export const getSchedules = async (req, res) => {
-
+export const getSchedules = async (req, res, next) => {
   try {
-      const total = await ScheduleModel.countDocuments();
-      const list = await ScheduleModel.find();
+    const total = await ScheduleModel.countDocuments();
 
-      return res.status(200).json({total: total, schedules: list});
+    if(total === 0) 
+      return res.sendStatus(204);
+
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 10;
+
+    const list = await ScheduleModel.find().limit(pageSize).skip(pageSize * page).sort({updated: "asc"});
+
+    return res.status(200).json({
+      total: total,
+      page: page,
+      schedules: list
+    });
   } catch (err) {
-      console.log(err);
+    next(err);
   }
 };
 
-export const getSchedule = (req, res) => {
-  ScheduleModel.findOne({id: req.id, active: true}).then((response) => {
-    return res.status(200).json(response);
-  });
+export const getSchedule = async (req, res, next) => {
+  try {
+    console.info(`GET Schedule :id ${req.params.id}`);
+    
+    const item = await ScheduleModel.findOne({
+      _id: req.params.id,
+      active: true
+    }, '_id name schedules updated');
+
+    if(item) {
+      return res.status(200).json(item);  
+    } else {
+      return res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const deleteSchedule = (req, res, next) => {
-  res.sendStatus(200);
-  next();
+export const deleteSchedule = async (req, res, next) => {
+  
+  try {
+    const resp = await ScheduleModel.findOneAndDelete({_id: req.params.id});
+    console.info(`delete: ${resp}`);
+    res.sendStatus(200);
+
+  } catch(err) {
+    next(err);
+  }
 };
 
 export const postSchedule = (req, res, next) => {
@@ -32,9 +62,12 @@ export const postSchedule = (req, res, next) => {
       return res.status(201).json(newSchedule);
     })
     .catch((err) => {
-      if(err.name === 'ValidationError') {
-        return res.status(400).json({code: err.name, message: err.message});
-      } 
+      if (err.name === 'ValidationError') {
+        return res.status(400).json({
+          code: err.name,
+          message: err.message
+        });
+      }
 
       next(err);
     });
